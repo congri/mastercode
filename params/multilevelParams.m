112a15b0af8279da67e67a58d0e1725c7f3e3978
Constantin Grigo <constantin.grigo@tum.de>
2016-06-30 22:42:39 +0200

    target temperature measurements are now exactly at the FEM nodal coordinates

--- params/multilevelParams.m
+++ params/multilevelParams.m
@@ -1,6 +1,6 @@
 %params specificially for the multi level model
 
-plt = 1        %plotu_s - u_f regression?
+plt = 0        %plotu_s - u_f regression?
 ck = 0         %check?
 genTrainingData = 1        %generate new accurate data?
 genTestData = 1            %generate new approximate data?
@@ -9,7 +9,7 @@ nSurrogates = 3;
 
 nData = 50;                %number of training data
 nTest = 50;                %number of test data
-nDataFast = 2e3;           %number of fast solver iterations for training data; to construct equally spaced training data
+nDataFast = 5e3;           %number of fast solver iterations for training data; to construct equally spaced training data
 upperFraction = .5;       %only take upper fraction of fast solver data to build surrogate, as we mainly want to
                            %predict high values of u_s
 
